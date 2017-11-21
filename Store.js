e7aef752ef74bd21af8d1811e125a106b09a5f0a
Louis Cherel <cherel.louis@gmail.com>
2017-11-21 15:42:25 +0100

    write some tests

--- Store.js
+++ Store.js
@@ -1,14 +1,60 @@
+const User = require('./User.js')
+const ObjectId = require('mongoose').Types.ObjectId
+
 class Store {
   constructor () {
-
+    this.users = []
+    this.promises = []
+    this.waitingIds = []
   }
 
-  getOne (id) {
+  async getOne (id) {
+    if (!ObjectId.isValid(id)) {
+      throw new Error('id is not an objectid !!')
+    }
 
+    const index = this.waitingIds.indexOf(id.toString())
+
+    if (index >= 0) {
+      return this.promises[index]
+    }
+
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i]._id.toString() === id.toString()) {
+        return this.users[i]
+      }
+    }
+
+    const promise = User.findById(id)
+    .then((user) => {
+      this.users.push(user)
+      const index = this.waitingIds.indexOf(user._id.toString())
+      this.waitingIds.splice(index, 1)
+      this.promises.splice(index, 1)
+      return user
+    })
+
+    this.waitingIds.push(id.toString())
+    this.promises.push(promise)
+    return promise
   }
 
-  get (ids) {
+  async get (ids) {
+    if (!(ids instanceof Array)) {
+      throw new Error('ids is not an array')
+    }
 
+    for (let i = 0; i < ids.length; i++) {
+      if (!ObjectId.isValid(ids[i])) {
+        throw new Error(`index ${i} is not an objectid !!`)
+      }
+    }
+
+    const promise = User.find({_id: {$in: ids}})
+    .then((users) => {
+      this.users = this.users.concat(users)
+      return users
+    })
   }
 }
 
--- test/Store.spec.js
+++ test/Store.spec.js
@@ -2,15 +2,178 @@ const proxyquire = require('proxyquire')
 const expect = require('chai').expect
 const sinon = require('sinon')
 sinon.test = require('sinon-test')(sinon)
+const ObjectId = require('mongoose').Types.ObjectId
+
+const User = {
+  findById: async (id) => ({}),
+  find: async (ids) => ({})
+}
 
 describe('Store', () => {
-  it('should blablabla', async () => {
-    const Store = proxyquire('../Store.js', {})
+  describe('#getOne', function () {
+    it('should throw an error if param is not an ObjectId', async () => {
+      const Store = proxyquire('../Store.js', { './User.js': User })
 
-    const store = new Store()
+      const myStore = new Store()
 
-    expect(store).to.be.instanceOf(Store)
-    return
+      try {
+        await myStore.getOne('1234567890')
+      } catch (err) {
+        expect(err).to.be.instanceOf(Error)
+        expect(err.message).to.equal('id is not an objectid !!')
+        return
+      }
+
+      throw new Error("getOne didn't throw an error")
+    })
+
+    it('should get the user with an id', sinon.test(async function () {
+      const userId = new ObjectId()
+
+      const UserMock = this.mock(User)
+      UserMock.expects('findById')
+        .once()
+        .withArgs(userId)
+        .resolves({ _id: userId })
+
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      const user = await myStore.getOne(userId)
+
+      expect(user).to.have.property('_id')
+      expect(user._id.toString()).to.equal(userId.toString())
+    }))
+
+    it('should get the user only once', sinon.test(async function () {
+      const userId = new ObjectId()
+
+      const UserMock = this.mock(User)
+      UserMock.expects('findById')
+        .once()
+        .withArgs(userId)
+        .resolves({ _id: userId })
+
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      await myStore.getOne(userId)
+      await myStore.getOne(userId)
+    }))
+
+    it('should get the user only once even if it is not resolved', sinon.test(async function () {
+      const userId = new ObjectId()
+
+      const UserMock = this.mock(User)
+      UserMock.expects('findById')
+        .once()
+        .withArgs(userId)
+        .resolves({ _id: userId })
+
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      myStore.getOne(userId)
+      const user = await myStore.getOne(userId)
+
+      expect(user).to.have.property('_id')
+      expect(user._id.toString()).to.equal(userId.toString())
+    }))
+  })
+
+  describe('#get', function () {
+    it('should throw an error if ids is not an Array', async () => {
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      try {
+        await myStore.get()
+      } catch (err) {
+        expect(err).to.be.instanceOf(Error)
+        expect(err.message).to.equal('ids is not an array')
+        return
+      }
+
+      throw new Error("get didn't throw an error")
+    })
+
+    it('should throw an error if param is not an ObjectId', async () => {
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      const usersIds = [new ObjectId(), '1234567890']
+
+      try {
+        await myStore.get(usersIds)
+      } catch (err) {
+        expect(err).to.be.instanceOf(Error)
+        expect(err.message).to.equal('index 1 is not an objectid !!')
+        return
+      }
+
+      throw new Error("get didn't throw an error")
+    })
+
+    it.skip('should get the user with an id', sinon.test(async function () {
+      const userId = new ObjectId()
+
+      const UserMock = this.mock(User)
+      UserMock.expects('findById')
+        .once()
+        .withArgs(userId)
+        .resolves({ _id: userId })
+
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      const user = await myStore.getOne(userId)
+
+      expect(user).to.have.property('_id')
+      expect(user._id.toString()).to.equal(userId.toString())
+    }))
+
+    it.skip('should get the user only once', sinon.test(async function () {
+      const userId = new ObjectId()
+
+      const UserMock = this.mock(User)
+      UserMock.expects('findById')
+        .once()
+        .withArgs(userId)
+        .resolves({ _id: userId })
+
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      await myStore.getOne(userId)
+      await myStore.getOne(userId)
+    }))
+
+    it.skip('should get the user only once even if it is not resolved', sinon.test(async function () {
+      const userId = new ObjectId()
+
+      const UserMock = this.mock(User)
+      UserMock.expects('findById')
+        .once()
+        .withArgs(userId)
+        .resolves({ _id: userId })
+
+      const Store = proxyquire('../Store.js', { './User.js': User })
+
+      const myStore = new Store()
+
+      myStore.getOne(userId)
+      const user = await myStore.getOne(userId)
+
+      expect(user).to.have.property('_id')
+      expect(user._id.toString()).to.equal(userId.toString())
+    }))
   })
 })
 
